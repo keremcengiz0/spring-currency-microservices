a3cf75e41c91e9b71eaee95032e81028a86ae210
keremcengiz0 <keremcengiz0@hotmail.com>
2023-06-06 16:04:36 +0300

    added gateway routers.

---
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.in28minutes.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
--- readme.md
+++ readme.md
@@ -12,3 +12,10 @@
 - http://localhost:8765/currency-exchange/currency-exchange/from/USD/to/INR
 - http://localhost:8765/currency-conversion/currency-conversion/from/USD/to/INR/quantity/10
 - http://localhost:8765/currency-conversion/currency-conversion-feign/from/USD/to/INR/quantity/10
+
+### Routers
+- http://localhost:8765/currency-exchange/from/USD/to/INR
+- http://localhost:8765/currency-conversion/from/USD/to/INR/quantity/10
+- http://localhost:8765/currency-conversion-feign/from/USD/to/INR/quantity/10
+
+
